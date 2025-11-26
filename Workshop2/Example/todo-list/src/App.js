3c7468ceb4a1674decd3fa92cb5de17a89b9951d
Anunnaki <92318629+AliTigui@users.noreply.github.com>
2025-11-26 21:04:54 +0100

    Refactor todo list to use useReducer
    Refactor todo list to use useReducer for state management. Implement reducer function for adding and deleting tasks.

--- Workshop2/Example/todo-list/src/App.js
+++ Workshop2/Example/todo-list/src/App.js
@@ -1,66 +1,91 @@
-import {useState} from 'react';
 import './App.css';
+import { useState, useReducer} from 'react';
 
 
+function reducer(state,action){
+  switch(action.type){
+    case 'ADD_TASK':
+      return [...state,action.payload];
+    case 'DELETE_TASK':
+      return state.filter((task)=>task.title!==action.payload);
+    default:
+      return state;
+  }
+}
 
-function Input(props){
+function Todo({children}){
+  return (
+    <div className="Todo">
+      <h1>Todo List</h1>
+      {children}
+     </div>
+
+    )
+}
+function Form({setTasks}){
     const [title,setTitle] = useState("");
     const [description,setDescription] = useState("");
     return(
-    <form onSubmit={
-    (e)=>{
-        e.preventDefault()
-        const newTask = {"title":title,"description":description}
-        props.setTasks([...props.tasks,newTask])
-        setTitle("")
-        setDescription("")
-        }
-    }>
+    <form>
     <label> Title:
     <input value={title} onChange={(e)=>{setTitle(e.target.value)}} />
     </label>
-    
+
     <label>Description:
     <textarea value={description} onChange={(e)=>{setDescription(e.target.value)}}>
     </textarea>
     </label>
     
-	<button type="submit">Add Task</button>
+    <button  class = "green" onClick={
+      (e)=>{
+        e.preventDefault();
+        const newTask = {"title":title,"description":description}
+        setTasks({type:"ADD_TASK",payload:newTask})
+        setTitle("")
+        setDescription("")
+        }
+    
+    }>Add Task</button>
     </form>
-)
+    )
 }
-function Tasks(props){
+function Task({title,description,setTasks}){
+  return (
+    <div key={title}>
+    <h3>{title}</h3>
+    <p>{description}</p>
+    <button onClick={()=>{
+          setTasks({type:"DELETE_TASK",payload:title})
+        }}>Delete Task</button>
+    </div>)
 
-	return (
-	<div>
-    {	
-	props.tasks.map((task)=>{
-     return (<div key="">
-		<h3> {task.title}</h3>
-		<p>{task.description}</p>
-		<button onClick={()=>{
-			const filteredTasks = props.tasks.filter(t=>t!==task)
-			props.setTasks(filteredTasks)
-		}}>Delete Task</button>
-  	</div>)
-	})
 }
-	</div>
-	)	
+function Tasks({tasks,setTasks}){
+    return (
+    <div>
+    {  
+    tasks.map((task,index)=>
+     <Task 
+      setTasks={setTasks}
+      key={index} 
+      title={task.title}
+      description={task.description} /> 
+    )
+    }
+    </div>
+    )  
 }
-
 
 function App() {
-  const [tasks,setTasks] = useState([]);
-  
+  const [tasks,setTasks] = useReducer(reducer,[]);
   return (
-    
-    <div className="App">
-      <h1>Todo List</h1>
-      <Input tasks={tasks} setTasks={setTasks}/>
+      <Todo >
+      <Form tasks={tasks} setTasks={setTasks} />
       <Tasks tasks={tasks} setTasks={setTasks}/>
-    </div>
+      </Todo>
+
   );
+
 }
 
 export default App;
